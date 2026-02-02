6d17ae7d9178950e54fce587f2e57cfb3de5f1e3
mohansathiamoorthy007-cyber <mohansathiamoorthy007@gmail.com>
2026-02-02 11:52:48 +0530

    Completed ROS2 C++ publisher node

---
+++ build/ros2_publisher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mohan/assignment-ros2-c-publisher-node-mohansathiamoorthy007-cyber
+# Build directory: /home/mohan/assignment-ros2-c-publisher-node-mohansathiamoorthy007-cyber/build/ros2_publisher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
