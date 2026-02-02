572ea1d22fd6713b88c19468203e99a3f90e9e43
mohansathiamoorthy007-cyber <mohansathiamoorthy007@gmail.com>
2026-02-02 11:57:17 +0530

    Remove build artifacts and add gitignore

--- build/ros2_publisher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mohan/assignment-ros2-c-publisher-node-mohansathiamoorthy007-cyber
-# Build directory: /home/mohan/assignment-ros2-c-publisher-node-mohansathiamoorthy007-cyber/build/ros2_publisher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
